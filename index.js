06fcfc1c67bb042af054e2f0a6c0e034ca7ab532
Eliot Uriarte <ueliot@yahoo.com>
2020-12-10 00:27:40 +0100

    all test for vue

--- index.js
+++ index.js
@@ -18,5 +18,65 @@ var app =new Vue({
     }
   })
 
+  var app3 = new Vue({
+    el: '#app-3',
+    data: {
+      seen: true
+    }
+  })
+
+  var app4 = new Vue({
+    el: '#app-4',
+    data: {
+      todos: [
+        { text: 'Aprender JavaScript' },
+        { text: 'Aprender Vue' },
+        { text: 'Construir algo increíble' }
+      ]
+    }
+  })
+
+  var app5 = new Vue({
+    el: '#app-5',
+    data: {
+      message: 'Hola Vue.js!'
+    },
+    methods: {
+      reverseMessage: function () {
+        this.message = this.message.split('').reverse().join('')
+      }
+    }
+  })
+
+  var app6 = new Vue({
+    el: '#app-6',
+    data: {
+      message: 'Hola Vue!'
+    }
+  })
+
+//#############Components in VUEJS######################
+  Vue.component('todo-item', {
+    props: ['todo'],
+    template: '<li>{{ todo.text }}</li>'
+  })
+  
+  var app7 = new Vue({
+    el: '#app-7',
+    data: {
+      groceryList: [
+        { id: 0, text: 'Vegetales' },
+        { id: 1, text: 'Queso' },
+        { id: 2, text: 'Cualquier otra cosa que se supone que los humanos coman' }
+      ]
+    }
+  })
+
+  //######################################################
+
+  
+
+  
+
   app.message="Interesante";
 
