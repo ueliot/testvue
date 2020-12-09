69fb96c850404ce5c0fdf4a73ebe1fa936fe3715
Eliot Uriarte <ueliot@yahoo.com>
2020-12-09 23:36:50 +0100

    First step for vue app

---
+++ index.js
@@ -0,0 +1,14 @@
+var app =new Vue({
+            el: '#app',
+            data: {  
+              message: 'Hello Vue.js!',
+              test: 'desde la web',
+              other: 'EXPER MODE'
+            }
+            /* for (const i in data){
+            console log (`${i}: ${data[i]}`)
+            } */
+    
+  });
+
+app.other = "XXXXX";
